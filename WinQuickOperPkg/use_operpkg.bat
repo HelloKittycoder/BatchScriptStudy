4312b2083dba0a759828d3b40360d3388d53462f
HelloKittycoder <shucheng2015@outlook.com>
2019-09-02 22:50:59 +0800

    设置临时PATH

--- WinQuickOperPkg/use_operpkg.bat
+++ WinQuickOperPkg/use_operpkg.bat
@@ -5,4 +5,6 @@ rem batool目录
 set BATOOL_PATH=%PKG_PATH%batool
 rem link目录
 set LINK_PATH=%PKG_PATH%link
+rem 设置临时PATH，该设置只在本次cmd窗口以及子窗口中有效，脚本结束或窗口关闭后失效
+set PATH=%BATOOL_PATH%;%LINK_PATH%;%PATH%
 start "windows操作工具" cmd
