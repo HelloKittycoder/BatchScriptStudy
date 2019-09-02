358bbf8d04e994a2e54e710320516583f2e9056f
HelloKittycoder <shucheng2015@outlook.com>
2019-09-02 22:20:44 +0800

    添加window操作工具包使用入口use_operpkg以及相关说明

--- WinQuickOperPkg/README.md
+++ WinQuickOperPkg/README.md
@@ -1,1 +1,5 @@
 ### Windows下常用操作工具包
+使用说明：  
+想要调用WinQuickOperPkg下的所有命令，有两种方法，  
+1. 把下面的目录（batool，link等）添加到环境变量中  
+2. 在调用完use_operpkg.bat后，再调用工具包里的命令
---
+++ WinQuickOperPkg/use_operpkg.bat
@@ -0,0 +1,8 @@
+@echo off
+rem 工具包所在目录
+set PKG_PATH=%~dp0
+rem batool目录
+set BATOOL_PATH=%PKG_PATH%batool
+rem link目录
+set LINK_PATH=%PKG_PATH%link
+start "windows操作工具" cmd
